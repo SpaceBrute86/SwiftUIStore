b57c658cd061599d94db114ec8076f12a1975a58
Bobbie Markwick <rnmarkwick@gmail.com>
2022-07-01 14:37:09 -0400

    Imported ads

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
        //  .package(url: "https://github.com/SpaceBrute86/SwiftUIHelpers", from: "1.0.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",  from: "9.6.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
---
+++ Sources/SwiftUIStore/Ads.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bobbie Markwick on 1/07/22.
+//
+
+import Foundation
+import GoogleMobileAds
+
