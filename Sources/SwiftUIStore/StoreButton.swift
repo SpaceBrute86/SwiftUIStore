03c9afabe696adafc06892f062e510b14e7088de
Bobbie Markwick <rnmarkwick@gmail.com>
2023-04-20 22:22:22 -0400

    Option to customise store button

--- Sources/SwiftUIStore/StoreButton.swift
+++ Sources/SwiftUIStore/StoreButton.swift
@@ -5,11 +5,21 @@ import StoreKit
 public struct StoreButton:View{
     @State private var showingPurchase = false
     
+    @State private var titleString:String
+    @State private var imageName:String
+
+    
     private var externals:[ExternalProduct]
-    public init(externalProducts:[ExternalProduct]){ externals = externalProducts }
+    public init(externalProducts:[ExternalProduct], title:String = "", systemImage:String = "cart"){
+        externals = externalProducts;
+        titleString = title
+        imageName = systemImage
+    }
     
     public var body: some View {
-        Button(action: { showingPurchase = true }, label: {Image(systemName: "cart")}).font(.title2).padding().sheet(isPresented: $showingPurchase){
+        Button(action: { showingPurchase = true }, label: {
+            if !titleString.isEmpty {  Text(titleString) } else { Image(systemName: imageName) }
+        }).font(.title2).padding().sheet(isPresented: $showingPurchase){
             NavigationView{ StoreView(externalProducts: externals) }
         }
     }
